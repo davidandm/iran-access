2b0bb7f59871616f2b874e2f4bd16bcd5d937b5f
davidandm <156902418+davidandm@users.noreply.github.com>
2024-01-17 13:42:14 +0330

    Update default.sh

--- default.sh
+++ default.sh
@@ -4,7 +4,7 @@ mkdir -p /etc/
 cd /etc/
 rm hosts.allow
 rm hosts.deny
-wget 
+wget https://raw.githubusercontent.com/davidandm/iran-access/main/default/hosts.allow
 wget https://raw.githubusercontent.com/davidandm/iran-access/main/default/hosts.deny
 sleep 2s
 sudo service ssh restart
